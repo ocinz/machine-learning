acfe21983c23201dd55142738707397bf2f32ba5
Ahmad Hafid (Ocinz) <ahmadhafid28632@gmail.com>
2024-09-29 16:14:47 +0700

    learned about logistic regeression, linear regression and K-Means Clustering

--- src/Machine Learning with Python for Beginner.py
+++ src/Machine Learning with Python for Beginner.py
@@ -290,29 +290,154 @@ dataset.fillna(dataset.mean(numeric_only=True), inplace = True)
 
 
 
-import pandas as pd
-dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/online_raw.csv')
-dataset.fillna(dataset.mean(numeric_only=True), inplace = True)
+# import pandas as pd
+# dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/online_raw.csv')
+# dataset.fillna(dataset.mean(numeric_only=True), inplace = True)
 
-from sklearn.preprocessing import LabelEncoder
-LE = LabelEncoder()
-dataset['Month'] = LE.fit_transform(dataset['Month'])
-LE = LabelEncoder()
-dataset['VisitorType'] = LE.fit_transform(dataset['VisitorType'])
-X = dataset.drop(['Revenue'], axis = 1)
-y = dataset['Revenue']
+# from sklearn.preprocessing import LabelEncoder
+# LE = LabelEncoder()
+# dataset['Month'] = LE.fit_transform(dataset['Month'])
+# LE = LabelEncoder()
+# dataset['VisitorType'] = LE.fit_transform(dataset['VisitorType'])
+# X = dataset.drop(['Revenue'], axis = 1)
+# y = dataset['Revenue']
 
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier 
+# from sklearn.model_selection import train_test_split
+# from sklearn.tree import DecisionTreeClassifier 
 
-# splitting the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+# # splitting the data
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
-# Call the classifier
-decision_tree = DecisionTreeClassifier()
-# Fit the classifier to the training data
-decision_tree = decision_tree.fit(X_train, y_train)
+# # Call the classifier
+# decision_tree = DecisionTreeClassifier()
+# # Fit the classifier to the training data
+# decision_tree = decision_tree.fit(X_train, y_train)
 
-# evaluating the decision_tree performance
-print('Training Accuracy :', decision_tree.score(X_train, y_train))
-print('Testing Accuracy :', decision_tree.score(X_test, y_test))
+# # evaluating the decision_tree performance
+# print('Training Accuracy :', decision_tree.score(X_train, y_train))
+# print('Testing Accuracy :', decision_tree.score(X_test, y_test))
+
+
+#############
+
+# #load dataset
+# import pandas as pd
+# housing = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/housing_boston.csv')
+# #Data rescaling
+# from sklearn import preprocessing
+# data_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+# housing[['RM','LSTAT','PTRATIO','MEDV']] = data_scaler.fit_transform(housing[['RM','LSTAT','PTRATIO','MEDV']])
+# # getting dependent and independent variables
+# X = housing.drop(['MEDV'], axis = 1)
+# y = housing['MEDV']
+# # checking the shapes
+# print('Shape of X:', X.shape)
+# print('Shape of y:', y.shape)
+
+# # splitting the data
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+# # checking the shapes  
+# print('Shape of X_train :', X_train.shape)
+# print('Shape of y_train :', y_train.shape)
+# print('Shape of X_test :', X_test.shape)
+# print('Shape of y_test :', y_test.shape)
+
+# ##import regressor from Scikit-Learn
+# from sklearn.linear_model import LinearRegression
+# # Call the regressor
+# reg = LinearRegression()
+# # Fit the regressor to the training data  
+# reg = reg.fit(X_train, y_train)
+# # Apply the regressor/model to the test data  
+# y_pred = reg.predict(X_test) 
+
+
+
+# import pandas as pd
+# housing = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/housing_boston.csv')
+
+# from sklearn import preprocessing
+# data_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+# housing[['RM','LSTAT','PTRATIO','MEDV']] = data_scaler.fit_transform(housing[['RM','LSTAT','PTRATIO','MEDV']])
+# X = housing.drop(['MEDV'], axis = 1)
+# y = housing['MEDV']
+
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+# from sklearn.linear_model import LinearRegression
+# reg = LinearRegression()
+# reg = reg.fit(X_train,y_train)
+# y_pred = reg.predict(X_test)
+
+# from sklearn.metrics import mean_squared_error, mean_absolute_error   
+# import numpy as np
+# import matplotlib.pyplot as plt 
+
+# #Calculating MSE, lower the value better it is. 0 means perfect prediction
+# mse = mean_squared_error(y_test, y_pred)
+# print('Mean squared error of testing set:', mse)
+# #Calculating MAE
+# mae = mean_absolute_error(y_test, y_pred)
+# print('Mean absolute error of testing set:', mae)
+# #Calculating RMSE
+# rmse = np.sqrt(mse)
+# print('Root Mean Squared Error of testing set:', rmse)
+
+# #Plotting y_test dan y_pred
+# plt.scatter(y_test, y_pred, c = 'green')
+# plt.xlabel('Price Actual')
+# plt.ylabel('Predicted value')
+# plt.title('True value vs predicted value : Linear Regression')
+# plt.show()
+
+
+
+# #import library
+# import pandas as pd  
+# from sklearn.cluster import KMeans
+
+# #load dataset
+# dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/mall_customers.csv')
+
+# #selecting features  
+# X = dataset[['annual_income','spending_score']]  
+
+# #Define KMeans as cluster_model  
+# cluster_model = KMeans(n_clusters = 5, random_state = 24)  
+# labels = cluster_model.fit_predict(X)
+
+
+
+import pandas as pd  
+from sklearn.cluster import KMeans  
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/pythonTutorial/mall_customers.csv')   
+X = dataset[['annual_income','spending_score']]  
+
+cluster_model = KMeans(n_clusters = 5, random_state = 24)  
+labels = cluster_model.fit_predict(X)
+
+#import library
+import matplotlib.pyplot as plt
+
+#convert dataframe to array
+X = X.values
+#Separate X to xs and ys --> use for chart axis
+xs = X[:,0]
+ys = X[:,1]
+# Make a scatter plot of xs and ys, using labels to define the colors
+plt.scatter(xs,ys,c=labels, alpha=0.5)
+
+# Assign the cluster centers: centroids
+centroids = cluster_model.cluster_centers_
+# Assign the columns of centroids: centroids_x, centroids_y
+centroids_x = centroids[:,0]
+centroids_y = centroids[:,1]
+# Make a scatter plot of centroids_x and centroids_y
+plt.scatter(centroids_x,centroids_y,marker='D', s=50)
+plt.title('K Means Clustering', fontsize = 20)
+plt.xlabel('Annual Income')
+plt.ylabel('Spending Score')
+plt.show()
